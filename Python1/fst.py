33f28e87c69e90eac4dd7645977bf121d86a7816
Saloni Kumari <chaudharysaloni2510@gmail.com>
2024-12-06 10:30:20 +0530

    This is my first Damsharades Games

---
+++ Python1/fst.py
@@ -0,0 +1,37 @@
+# a=7
+# a=9
+# print(a)
+
+
+# print(not True)
+
+# #INPUT
+
+# name=("Enter your name: ")
+# print(name)
+
+# print("Hello,\nI am Saloni")
+
+# str1="Saloni"
+# str2="Kumari"
+# print(len(str1))
+# print(str1[5])
+# print(str1+" "+str2)
+
+# Swan="Ravi_CHaudhary"
+# print(Swan[-6:-2])
+# print(Swan.endswith("ry"))
+# print(Swan.capitalize())
+# print(Swan.replace("Ravi","Saloni"))
+# print(Swan.find("a"))
+# print(Swan.find("Chaudhary"))
+
+# light="red"
+# if(light=="red"):
+#     print("Stop")
+# elif(light=="green"):
+#  print("Go")
+# elif(light=="yellow"):
+#         print("look")
+# else:
+#         print("Invalid")
